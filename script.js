a4792005a0e9e6d67c12cee3612e6ed9b14097b8
Fernanda Dias <fernandadias.dev@gmail.com>
2023-06-05 17:25:12 -0300

    refactor: :recycle: estrutura os dados do modal com objetos literais

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
 
 const Modal = {
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    btnClose: document.querySelector('.modal button.close'),
+
     open() {
-        modalWrapper.classList.add('open')
+        Modal.wrapper.classList.add('open')
     },
     close() {
-        modalWrapper.classList.remove('open')
+        Modal.wrapper.classList.remove('open')
     }
 
 }
@@ -25,11 +26,11 @@ form.onsubmit = event => {
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
 
-    modalMessage.innerText = message
+    Modal.message.innerText = message
     Modal.open()
 }
 
-modalBtnClose.addEventListener('click', () => {
+Modal.btnClose.addEventListener('click', () => {
     Modal.close()
 })
 
